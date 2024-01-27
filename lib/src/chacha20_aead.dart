21d3b70b6c8808786d74a69ad619d33758dcbb8a
Carlos Makin <carlos@makin.dev>
2024-01-26 23:22:32 -0700

    Update chacha20_aead.dart

--- lib/src/chacha20_aead.dart
+++ lib/src/chacha20_aead.dart
@@ -30,25 +30,6 @@ Uint8List _buildMacData(Uint8List bytes, Uint8List? aad) {
   return macData;
 }
 
-/// Compares two lists of integers (`left` and `right`) for equality in a constant-time manner.
-/// This approach mitigates timing attacks in cryptographic contexts by ensuring the
-/// execution time depends only on the length of the lists, not the data they contain.
-///
-/// Returns `true` if the lists are equal, `false` otherwise.
-bool _secureEquals(List<int> left, List<int> right) {
-  // Return false immediately if lengths differ, as the lists can't be equal.
-  if (left.length != right.length) return false;
-
-  int result = 0;
-  // Compare elements using XOR; accumulate any differences in `result`.
-  for (int i = 0; i < left.length; i++) {
-    result |= (left[i] ^ right[i]);
-  }
-
-  // If `result` is 0, all elements matched; otherwise, at least one pair differed.
-  return result == 0;
-}
-
 /// ChaCha20-Poly1305 Authenticated Encryption with Associated Data (AEAD) as defined in RFC 8439.
 ///
 /// Combining ChaCha20 cipher and Poly1305 MAC, this class offers symmetric encryption ensuring
@@ -106,7 +87,7 @@ abstract class Chacha20Poly1305 {
     final Uint8List macData = _buildMacData(ciphertext, aad);
 
     // Calculate and verify the MAC tag
-    if (!_secureEquals(tag, Poly1305.computeMac(otk, macData))) {
+    if (Poly1305.verifyMac(otk, macData, tag)) {
       throw Exception('MAC verification failed.');
     }
 
