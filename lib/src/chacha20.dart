f95be22b0500e5e79e20b4af65a86330ad28fc2a
Carlos Makin <carlos@makin.dev>
2024-02-07 17:18:14 -0700

    Update chacha20.dart

--- lib/src/chacha20.dart
+++ lib/src/chacha20.dart
@@ -10,34 +10,36 @@ abstract class ChaCha20 {
   ///
   /// Accepts a 256-bit key, a 96-bit nonce, and an optional counter (default: 1).
   static encrypt(Uint8List key, Uint8List nonce, Uint8List data, [int counter = 1]) {
-    return _chacha20(key, nonce, data, counter);
+    if (key.length != 32) throw ArgumentError('Invalid key');
+    if (nonce.length != 12) throw ArgumentError('Invalid nonce');
+    return _chacha20(Uint32List.view(key.buffer), Uint32List.view(nonce.buffer), data, counter);
   }
 
   /// Decrypts data using ChaCha20 as per RFC 8439.
   ///
   /// Accepts a 256-bit key, a 96-bit nonce, and an optional counter (default: 1).
   static decrypt(Uint8List key, Uint8List nonce, Uint8List data, [int counter = 1]) {
-    return _chacha20(key, nonce, data, counter);
+    if (key.length != 32) throw ArgumentError('Invalid key');
+    if (nonce.length != 12) throw ArgumentError('Invalid nonce');
+    return _chacha20(Uint32List.view(key.buffer), Uint32List.view(nonce.buffer), data, counter);
   }
 }
 
 /// Encrypts or decrypts data using ChaCha20 algorithm, configurable for both encryption and decryption.
 /// Accepts a 256-bit key, a 96-bit nonce, data, and an optional counter (default: 1).
-Uint8List _chacha20(Uint8List key, Uint8List nonce, Uint8List data, [int counter = 1]) {
-  if (key.length != 32) throw ArgumentError('Invalid key');
-  if (nonce.length != 12) throw ArgumentError('Invalid nonce');
+Uint8List _chacha20(Uint32List key, Uint32List nonce, Uint8List data, [int counter = 1]) {
   if (data.length >= 274877906880) throw ArgumentError('Maximum size reached');
 
   final int dataSize = data.lengthInBytes;
   final Uint8List output = Uint8List(dataSize);
 
-  final Uint32List key32Bit = Uint32List.view(key.buffer);
-  final Uint32List nonce32Bit = Uint32List.view(nonce.buffer);
+  // Initialize the state with the constants, key, counter, and nonce
+  final Uint32List state = initState(key, nonce);
 
   // Encrypt each full block
   final int fullBlocks = dataSize ~/ 64;
   for (int j = 0; j < fullBlocks; j++) {
-    final Uint8List keyStream = chacha20Block(key32Bit, nonce32Bit, counter + j);
+    final Uint8List keyStream = chacha20Block(key, nonce, counter + j, state);
     for (int i = 0; i < 64; i++) {
       output[j * 64 + i] = data[j * 64 + i] ^ keyStream[i];
     }
@@ -46,7 +48,7 @@ Uint8List _chacha20(Uint8List key, Uint8List nonce, Uint8List data, [int counter
   // Handle any remaining partial block
   final int remaining = dataSize % 64;
   if (remaining != 0) {
-    final Uint8List keyStream = chacha20Block(key32Bit, nonce32Bit, counter + fullBlocks);
+    final Uint8List keyStream = chacha20Block(key, nonce, counter + fullBlocks, state);
     final int start = fullBlocks * 64;
     for (int i = 0; i < remaining; i++) {
       output[start + i] = data[start + i] ^ keyStream[i];
@@ -56,18 +58,8 @@ Uint8List _chacha20(Uint8List key, Uint8List nonce, Uint8List data, [int counter
   return output;
 }
 
-/// The ChaCha20 block function is the core of the ChaCha20 algorithm.
-/// The function transforms a ChaCha state by running multiple quarter rounds.
-///
-/// The inputs to ChaCha20 are:
-///
-/// - A 256-bit key, treated as a concatenation of eight 32-bit little-endian integers.
-/// - A 96-bit nonce, treated as a concatenation of three 32-bit little-endian integers
-/// - A 32-bit block count parameter, treated as a 32-bit little-endian integer.
-///
-///The output is 64 random-looking bytes.
-Uint8List chacha20Block(Uint32List key, Uint32List nonce, int counter) {
-  // Initialize the state with the constants, key, counter, and nonce
+// Initializes the state with the constants, key, and nonce
+Uint32List initState(Uint32List key, Uint32List nonce) {
   final Uint32List state = Uint32List(16);
 
   state[0] = 0x61707865;
@@ -82,10 +74,26 @@ Uint8List chacha20Block(Uint32List key, Uint32List nonce, int counter) {
   state[9] = key[5];
   state[10] = key[6];
   state[11] = key[7];
-  state[12] = counter;
   state[13] = nonce[0];
   state[14] = nonce[1];
   state[15] = nonce[2];
+
+  return state;
+}
+
+/// The ChaCha20 block function is the core of the ChaCha20 algorithm.
+/// The function transforms a ChaCha state by running multiple quarter rounds.
+///
+/// The inputs to ChaCha20 are:
+///
+/// - A 256-bit key, treated as a concatenation of eight 32-bit little-endian integers.
+/// - A 96-bit nonce, treated as a concatenation of three 32-bit little-endian integers
+/// - A 32-bit block count parameter, treated as a 32-bit little-endian integer.
+///
+///The output is 64 random-looking bytes.
+Uint8List chacha20Block(Uint32List key, Uint32List nonce, int counter, Uint32List state) {
+  // Initialize the state with the counter
+  state[12] = counter;
 
   // Flip endianness only if the system is big-endian
   _ensureLittleEndian(state);
