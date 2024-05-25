9fb19e0abfa2324fd91c8b614b366a34de726744
Carlos Makin <carlos@makin.dev>
2024-05-25 00:23:08 -0600

    Update chacha20_poly1305_benchmark.dart

--- benchmark/chacha20_poly1305_benchmark.dart
+++ benchmark/chacha20_poly1305_benchmark.dart
@@ -4,7 +4,7 @@ import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:chacha/export.dart';
 
 class ChaCha20Poly1305Benchmark extends BenchmarkBase {
-  const ChaCha20Poly1305Benchmark() : super('chacha20-poly1305');
+  const ChaCha20Poly1305Benchmark() : super('chacha20poly1305');
 
   static void main() => ChaCha20Poly1305Benchmark().report();
 
